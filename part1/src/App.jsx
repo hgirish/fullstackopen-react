66f23fd4f855b99e3fc83fe6613612eea63ff0ab
Girish <girish@post.com>
2024-07-02 11:09:36 -0700

    part1 end

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,32 @@
-const App = () => {
+const Hello = (props) => {
+  console.log(props);
   return (
     <div>
-      <p>Hello World</p>
+      <p>Hello {props.name}, You are {props.age} years old.</p>
     </div>
   )
+}
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+const App = () => {
+ const friends = [
+  {name: 'Peter', age:4},
+  {name: 'Maya', age: 10}
+ ];
+ 
+  return (
+  <>
+    <h1>Greeting</h1>
+    <Hello name={friends[0].name} age={friends[0].age} />
+    <Hello name={friends[1].name} age={friends[1].age} />
+    <Footer />
+    </>
+)
 }
 
 export default App
