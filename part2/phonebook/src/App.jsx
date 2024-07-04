facc5c4235f9d3504e78591fb6cafc20f8b35788
Girish <girish@post.com>
2024-07-04 15:34:15 -0700

    2.10: The Phonebook Step 5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 function App() {
   const [persons, setPersons] = useState([
@@ -49,24 +52,18 @@ function App() {
   return (  
       <div>
         <h2>Phonebook</h2>
-        <div>filter shown with <input onChange={handleFilterChange} /></div>
+
+       <Filter handleFilterChange={handleFilterChange} />
+
         <h2>add a new</h2>
-        <form onSubmit={addNote}>
-          <div>
-            name: <input value={newName} onChange={handleNameChange} />
-          </div>
-          <div>
-            number: <input value={newNumber} onChange={handleNumberChange} />
-          </div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-        </form>
+
+        <PersonForm addNote={addNote} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
+  
         <h2>Numbers</h2>
+
+        <Persons personsToShow={personsToShow} />
        
-       <ul>
-        {personsToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
-       </ul>   
+   
   
   
       </div> 
