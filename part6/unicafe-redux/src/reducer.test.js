3044b352ef44a52cbe1b56abe4950d512a552155
Girish <girish@post.com>
2025-01-20 19:15:10 -0800

    implement GOOD action in counter reducer and add corresponding test

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -1,3 +1,4 @@
+import deepFreeze from "deep-freeze"
 import counterReducer from "./reducer"
 
 describe('unicafe reducer', () => {
@@ -15,5 +16,20 @@ describe('unicafe reducer', () => {
 
         const newState = counterReducer(undefined, action)
         expect(newState).toEqual(initialState)
+    })
+
+    test('good is incremented', () => {
+        const action = {
+            type: 'GOOD'
+        }
+
+        const state = initialState
+        deepFreeze (state)
+        const newState = counterReducer(state, action)
+        expect(newState).toEqual({
+            good: 1,
+            bad: 0,
+            ok: 0
+        })
     })
 })
