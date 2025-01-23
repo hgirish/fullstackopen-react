4deb1aaf26920cacc850cde635e0b19229770b14
Girish <girish@post.com>
2025-01-23 14:12:24 -0800

    refactor: separate AnecdoteForm and AnecdoteList components from App

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,27 @@
+
+import { useDispatch } from 'react-redux';
+import { createAnecdote } from '../reducers/anecdoteReducer';
+
+const AnecdoteForm = () => { 
+    const dispatch  = useDispatch()
+
+    const addAnecdote = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        dispatch(createAnecdote(content))
+      }
+
+    return (
+        <div>
+             <h2>create new</h2>
+        <form onSubmit={addAnecdote}>
+        <div><input name="anecdote" /></div>
+        <button type='submit'>create</button>
+      </form>
+      </div>
+    )
+}
+
+
+export default AnecdoteForm // This is the default export of the AnecdoteForm component. It is exported so that it can be imported and used in other components.
