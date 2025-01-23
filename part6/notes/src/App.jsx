4415e85c6ba5050e6bde87174975b426cce9fbaf
Girish <girish@post.com>
2025-01-22 17:34:46 -0800

    add react-redux integration for note management and implement note creation and importance toggle features

--- part6/notes/src/App.jsx
+++ part6/notes/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { createNote, toggleImportanceOf } from './reducers/note-reducer'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+
+const toggleImportance = (id) => {
+  dispatch(toggleImportanceOf(id))
+}
+
+  const addNote = (event) => {
+      event.preventDefault()
+      const content = event.target.note.value 
+      event.target.note.value = ''
+      dispatch(createNote(content))
+  }
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <form onSubmit={addNote}>
+              <input name="note" />
+              <button type="submit">add</button>
+          </form>
+          <ul>
+              {notes.map(note => 
+                  <li key={note.id} onClick={()=> toggleImportance(note.id)} >
+                      {note.content} <strong>{note.important ? 'important' : ''}</strong>
+                  </li>
+              )}
+          </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
+
 
 export default App
--- part6/notes/src/main.jsx
+++ part6/notes/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import noteReducer from './reducers/note-reducer.jsx'
+
+const store = createStore(noteReducer)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
