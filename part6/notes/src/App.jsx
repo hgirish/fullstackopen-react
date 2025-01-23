b6c828a39b367d05331019c22e5d8774e4d4ac0f
Girish <girish@post.com>
2025-01-22 17:51:11 -0800

    replace Note component with Notes component in App component

--- part6/notes/src/App.jsx
+++ part6/notes/src/App.jsx
@@ -1,24 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
-import { createNote, toggleImportanceOf } from './reducers/note-reducer'
 import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 
 const App = () => {
 
 
-  const dispatch = useDispatch()
-  const notes = useSelector(state => state)
 
-const toggleImportance = (id) => {
-  dispatch(toggleImportanceOf(id))
-}
 
 
 
   return (
       <div>
           <NewNote />
-       
+       <Notes />
       </div>
   )
 }
