d139d0c4f514ec2b205421c41ddc03204c0f3f91
Girish <girish@post.com>
2025-01-25 18:04:05 -0800

    :hammer: refactor notificationSlice to improve code consistency and formatting

--- part6/redux-anecdotes/src/reducers/notificationSlice.js
+++ part6/redux-anecdotes/src/reducers/notificationSlice.js
@@ -1,26 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const notificationSlice = createSlice({
     name: 'notification',
     initialState: '',
     reducers: {
-        setNotificationInternal(state, action) {           
-            return action.payload
+        setNotificationInternal(state, action) {
+            return action.payload;
         },
         removeNotification() {
-            return ''
+            return '';
         }
     }
-})
-export const { setNotificationInternal, removeNotification} = notificationSlice.actions
-export const setNotification = (content,timeoutInSeconds) => {
-   
-    return async dispatch => {
-        setTimeout(() => {
-            dispatch(removeNotification())
-        }, timeoutInSeconds * 1000)
-        dispatch(setNotificationInternal(content))
-    }
-}
+});
 
-export default notificationSlice.reducer
+export const { setNotificationInternal, removeNotification } = notificationSlice.actions;
+
+export const setNotification = (content, timeoutInSeconds) => {
+    return dispatch => {
+        dispatch(setNotificationInternal(content));
+        setTimeout(() => {
+            dispatch(removeNotification());
+        }, timeoutInSeconds * 1000);
+    };
+};
+
+export default notificationSlice.reducer;
