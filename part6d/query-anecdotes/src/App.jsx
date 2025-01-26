d234348fa33c1ac2adc2877f8ddbf3d530fdab85
Girish <girish@post.com>
2025-01-26 13:54:05 -0800

    :sparkles: integrate React Query for anecdote fetching and update axios to latest version

--- part6d/query-anecdotes/src/App.jsx
+++ part6d/query-anecdotes/src/App.jsx
@@ -1,5 +1,7 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
 
 const App = () => {
 
@@ -7,13 +9,20 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(response => response.data),
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)));
+  
+  if (result.isLoading){
+    return <div>
+      <h2>anecdote service not available due to problems in server</h2>
+    </div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
